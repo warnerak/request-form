a465e371c15466c3194443031e96e3c6e0d6c23b
unknown <warnerak@dukes.jmu.edu>
2019-06-04 17:53:57 -0400

    opening heroku

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "N*ex7USt",
-    database: "gis_request"
-});
+if (process.env.JAWSDB_URL) {
+    connections = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "N*ex7USt",
+        database: "gis_request"
+    });
+};
+
 
 connection.connect(function (err) {
     if (err) {
@@ -15,5 +21,7 @@ connection.connect(function (err) {
     }
     console.log("connected as id " + connection.threadId);
 });
+
+
 
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -5,18 +5,29 @@ USE gis_request;
 
 CREATE TABLE requests
 (
-	id int NOT NULL AUTO_INCREMENT,
-    gis_analyst varchar (255),
-	project_manager varchar (255) NOT NULL,
-    project_priority varchar (10),
+    id int NOT NULL
+    AUTO_INCREMENT,
+    gis_analyst varchar
+    (255),
+	project_manager varchar
+    (255) NOT NULL,
+    project_priority varchar
+    (10),
     date_submitted DATE,
     due_date DATE,
-    project_name varchar(255) NOT NULL,
-    project_number NUMERIC (10, 2) NOT NULL,
-    latitude NUMERIC (10, 6) NOT NULL,
-    longitude NUMERIC (10, 6) NOT NULL,
-    notes_comments varchar (500),
-    map_requested varchar (255) NOT NULL,
+    project_name varchar
+    (255) NOT NULL,
+    project_number NUMERIC
+    (10, 2) NOT NULL,
+    latitude NUMERIC
+    (10, 6) NOT NULL,
+    longitude NUMERIC
+    (10, 6) NOT NULL,
+    notes_comments varchar
+    (500),
+    map_requested varchar
+    (255) NOT NULL,
     active_archive BOOLEAN DEFAULT TRUE,
-	PRIMARY KEY (id)
+	PRIMARY KEY
+    (id)
 );
