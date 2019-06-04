26d8c9e9509e9b76e26bd97f27fd5d987b688caf
unknown <warnerak@dukes.jmu.edu>
2019-06-04 18:11:05 -0400

    heroku push

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connections = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     var connection = mysql.createConnection({
         host: "localhost",
